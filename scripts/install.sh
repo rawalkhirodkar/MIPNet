029c71526c24b03496550ff2918eea9b7b2562d9
Rawal Khirodkar <rawalkhirodkar@gmail.com>
2022-02-03 04:53:38 -0500

    Update install

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,19 @@ cd ..
 
 conda create -n mipnet python=3.8 -y
 conda activate mipnet
-conda install pytorch torchvision cudatoolkit=10.1 -c pytorch -y
+# conda install pytorch torchvision cudatoolkit=10.1 -c pytorch -y
+conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=11.1 -c pytorch -c conda-forge -y ### for new gpus
 pip install Cython h5py
 pip install -r requirements.txt
+
+## install crowdpose
+cd lib/crowdpose-api/crowdpose-api/PythonAPI
+./install.sh
+cd ../../../..
+
+## install nms
+cd lib/nms
+make clean
+make
+
+cd ../..
